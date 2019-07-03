06a485c7955d22c7582ce145acd63b43c76a8b2d
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-07-03 13:41:07 +0530

    Update lol.py

--- stdplugins/lol.py
+++ stdplugins/lol.py
@@ -55,7 +55,7 @@ async def _(event):
             "Loooooooooooooooooooooo",
             "Loooooooooooooooooooooooo",
             "LooooooooooooooooooooooooooL"
-            "Kyaaaaaaaaaaaakabbbseloooldekkkkkmrhaaahaaaii"
+            "निकललवडेपहलीफुरसतमेनिकल👌👌👌"
         ]
 
         for i in animation_ttl:
