377d7fb0660176c643c0520981c2cf09d9130f0a
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-07-21 08:19:26 +0530

    Update fullgali2.py

--- stdplugins/fullgali2.py
+++ stdplugins/fullgali2.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("gaali2"))
+@borg.on(admin_cmd("gali"))
 async def _(event):
     if event.fwd_from:
         return
