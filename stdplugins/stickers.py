642bbccdab32ff8bda2ca010af06ffa2628388bc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-05 22:57:23 +0530

    Update stickers.py
    😂

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -118,7 +118,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"sticker added! Your pack can be found [here](t.me/addstickers/{packshortname})")
+    await event.edit(f"Muordered this sticker4! Sticker soul is [here](t.me/addstickers/{packshortname})")
 
 
 @borg.on(admin_cmd("packinfo"))
