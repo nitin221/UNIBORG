8bd19fae52f6725a3d520f297cfce39c26d8c3ce
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-23 07:24:53 +0530

    Update sp_search.py

--- stdplugins/sp_search.py
+++ stdplugins/sp_search.py
@@ -11,7 +11,7 @@ from uniborg.util import admin_cmd
 
 
 
-@borg.on(admin_cmd(pattern="sp ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="sch ?(.*)", allow_sudo=True))
 async def sp_search(event):
 	search_str = event.pattern_match.group(1)
 
