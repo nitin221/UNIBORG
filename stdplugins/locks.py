149fa564da722416f0e5016997eb3e1019da80ef
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-28 12:29:37 +0530

    Update locks.py

--- stdplugins/locks.py
+++ stdplugins/locks.py
@@ -82,7 +82,7 @@ async def _(event):
             )
 
 
-@borg.on(admin_cmd("unlock ?(.*)"))
+@borg.on(admin_cmd(pattern="unlock ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
