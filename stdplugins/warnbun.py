c0661a3d94e4d9adb07c0fb93762b227a70629bc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-28 15:23:50 +0530

    Update warnbun.py

--- stdplugins/warnbun.py
+++ stdplugins/warnbun.py
@@ -51,7 +51,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(patter="warn3"))
+@borg.on(admin_cmd(pattern="warn3"))
 async def _(event):
     if event.fwd_from:
         return
