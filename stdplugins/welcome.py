e40204fd83259f14b525dcb332a3dc1f565b4e72
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-28 09:14:13 +0530

    Update welcome.py

--- stdplugins/welcome.py
+++ stdplugins/welcome.py
@@ -56,7 +56,7 @@ async def _(event):
             update_previous_welcome(event.chat_id, current_message.id)
 
 
-@borg.on(admin_cmd("savewelcome"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="savewelcome"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -71,7 +71,7 @@ async def _(event):
         await event.edit("Welcome Note Saved. ")
 
 
-@borg.on(admin_cmd("clearwelcome"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="clearwelcome"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
