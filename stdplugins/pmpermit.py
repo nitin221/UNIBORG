bb193970f0cb8bb86e96c5ab77d26293d0830ecc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-08-30 20:20:20 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -11,8 +11,8 @@ from sql_helpers.pmpermit_sql import is_approved, approve, disapprove, get_all_a
 from uniborg.util import admin_cmd
 
 
-borg.storage.PM_WARNS = {}
-borg.storage.PREV_REPLY_MESSAGE = {}
+PM_WARNS = {}
+PREV_REPLY_MESSAGE = {}
 
 
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
@@ -35,22 +35,22 @@ async def monito_p_m_s(event):
         chat = await event.get_chat()
         if not is_approved(chat.id) and chat.id != borg.uid:
             logger.info(chat.stringify())
-            logger.info(borg.storage.PM_WARNS)
-            if chat.id not in borg.storage.PM_WARNS:
-                borg.storage.PM_WARNS.update({chat.id: 0})
-            if borg.storage.PM_WARNS[chat.id] == Config.MAX_FLOOD_IN_P_M_s:
+            logger.info(PM_WARNS)
+            if chat.id not in PM_WARNS:
+                PM_WARNS.update({chat.id: 0})
+            if PM_WARNS[chat.id] == Config.MAX_FLOOD_IN_P_M_s:
                 r = await event.reply(UNIBORG_USER_BOT_WARN_ZERO)
                 await asyncio.sleep(3)
                 await borg(functions.contacts.BlockRequest(chat.id))
-                if chat.id in borg.storage.PREV_REPLY_MESSAGE:
-                    await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
-                borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
+                if chat.id in PREV_REPLY_MESSAGE:
+                    await PREV_REPLY_MESSAGE[chat.id].delete()
+                PREV_REPLY_MESSAGE[chat.id] = r
                 return
             r = await event.reply(UNIBORG_USER_BOT_NO_WARN)
-            borg.storage.PM_WARNS[chat.id] += 1
-            if chat.id in borg.storage.PREV_REPLY_MESSAGE:
-                await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
-            borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
+            PM_WARNS[chat.id] += 1
+            if chat.id in PREV_REPLY_MESSAGE:
+                await PREV_REPLY_MESSAGE[chat.id].delete()
+            PREV_REPLY_MESSAGE[chat.id] = r
 
 
 @borg.on(admin_cmd(pattern="pm ?(.*)"))
@@ -62,11 +62,12 @@ async def approve_p_m(event):
     if Config.NO_P_M_SPAM:
         if event.is_private:
             if not is_approved(chat.id):
-                if chat.id in borg.storage.PM_WARNS:
-                    del borg.storage.PM_WARNS[chat.id]
-                if chat.id in borg.storage.PREV_REPLY_MESSAGE:
-                    await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
-                    del borg.storage.PREV_REPLY_MESSAGE[chat.id]
+                if chat.id in PM_WARNS:
+                   
+                    del PM_WARNS[chat.id]
+                if chat.id in PREV_REPLY_MESSAGE:
+                    await PREV_REPLY_MESSAGE[chat.id].delete()
+                    del PREV_REPLY_MESSAGE[chat.id]
                 approve(chat.id, reason)
                 await event.edit("Private Message Accepted")
                 await asyncio.sleep(3)
