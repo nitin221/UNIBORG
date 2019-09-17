e50df9c47da7922ecbc545be0e85b1ec6b6b88a2
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-09-17 14:15:46 +0530

    Update get_admin.py

--- stdplugins/get_admin.py
+++ stdplugins/get_admin.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "**Admins in this Chat**: \n"
+    mentions = "**Admins in this Chat**: \n\n"
     should_mention_admins = False
     reply_message = None
     pattern_match_str = event.pattern_match.group(1)
@@ -23,7 +23,7 @@ async def _(event):
     if not input_str:
         chat = to_write_chat
     else:
-        mentions_heading = "Admins in {} channel: \n".format(input_str)
+        mentions_heading = "Admins in {} channel: \n\n".format(input_str)
         mentions = mentions_heading
         try:
             chat = await borg.get_entity(input_str)
