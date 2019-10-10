0eee0d3e2e8e47723c3f6def2e556c4d733d4de7
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-10 19:48:15 +0530

    Update autopp.py

--- stdplugins/autopp.py
+++ stdplugins/autopp.py
@@ -23,7 +23,7 @@ FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 @borg.on(admin_cmd(pattern="autopp ?(.*)"))
 async def autopic(event):
     downloaded_file_name = "./UniBorg/original_pic.png"
-    downloader = SmartDL(Config.RAVANA_LEELA, downloaded_file_name, progress_bar=True)
+    downloader = SmartDL(Config.AVATAR_XD, downloaded_file_name, progress_bar=True)
     downloader.start(blocking=False)
     photo = "photo_pfp.png"
     while not downloader.isFinished():
