2fd94e99aab5350c5d47162089e81dab81d3a83b
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-10 13:41:30 +0530

    Update invite.py

--- stdplugins/invite.py
+++ stdplugins/invite.py
@@ -5,7 +5,7 @@ from telethon import functions
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="invite ?(.*)" allow_sudo=True))
+@borg.on(admin_cmd(pattern="invite ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
