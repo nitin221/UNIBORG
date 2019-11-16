de9dcc856568414adc7701085db42f0813ca283e
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-16 12:15:38 +0530

    Update wh.py

--- stdplugins/wh.py
+++ stdplugins/wh.py
@@ -9,7 +9,8 @@ from uniborg.util import admin_cmd
 TMP_DOWNLOAD_DIRECTORY = "./"
 
 
-@register(pattern="^.wh(?: |$)(.*)", outgoing=True)
+
+@borg.on(admin_cmd(pattern="wh (.*)"))
 async def who(event):
     if event.fwd_from:
         return
@@ -89,7 +90,7 @@ async def fetch_info(replied_user, event):
         common_chat = "I've seen them in... Wow. Are they stalking me? "
         common_chat += "They're in all the same places I am... oh. It's me."
 
-    caption = "<b>USER INFO:</b> \n"
+    caption = "<b>General Info:</b> \n"
     caption += f"First Name: {first_name} \n"
     caption += f"Last Name: {last_name} \n"
     caption += f"Username: {username} \n"
@@ -98,8 +99,8 @@ async def fetch_info(replied_user, event):
     caption += f"Is Verified by Telegram: {verified} \n"
     caption += f"ID: <code>{user_id}</code> \n \n"
     caption += f"Bio: \n<code>{user_bio}</code> \n \n"
-    caption += f"Common Chats with this user: {common_chat} \n"
-    caption += f"Permanent Link To Profile: "
+    caption += f"Common Chats: {common_chat} \n"
+    caption += f"Permanent Link: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return caption
