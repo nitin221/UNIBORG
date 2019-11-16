531fd4953706485118fd0e77265f51cb9ea20a19
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-16 13:38:27 +0530

    Update wh.py

--- stdplugins/wh.py
+++ stdplugins/wh.py
@@ -1,5 +1,10 @@
-import os
+"""Get Telegram nformation
+Syntax: .wh @username/userid"""
 
+
+
+
+import os
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
@@ -92,7 +97,7 @@ async def fetch_info(replied_user, event):
         
     
     caption = "<b>General Info OF:</b> \n"
-    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
+    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"\n
     caption += f"<b>First Name</b>: {first_name} \n"
     caption += f"Last Name</b>: {last_name} \n"
     caption += f"<b>ID</b>: <code>{user_id}</code> \n \n"
