c7fc406d47092198ef5c9ba0a2fe8675217480e5
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-17 08:21:34 +0530

    Update wh.py

--- stdplugins/wh.py
+++ stdplugins/wh.py
@@ -125,7 +125,7 @@ async def fetch_info(replied_user, event):
     caption += f"DC ID: {dc_id}\n"
     caption += f"Number of PPs: {replied_user_profile_photos_count}\n"
     caption += f"Common Groups: {common_chat} \n \n"
-    caption += f"<b>Bio</b>: \n<code>{user_bio}</code>
+    caption += f"<b>Bio</b>: \n<code>{user_bio}</code> \n"
 
     return caption
     
