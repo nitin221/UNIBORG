2c593179cd607af2c296fd90fcf8ae6c8eb739da
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-18 10:09:47 +0530

    Update username.py

--- stdplugins/username.py
+++ stdplugins/username.py
@@ -16,9 +16,9 @@ async def update_username(username):
     newusername = username.pattern_match.group(1)
     try:
         await borg(functions.account.UpdateUsernameRequest(  # pylint:disable=E0602
-            about=username
         ))
         await username.edit("Succesfully changed my Username")
     except UsernameOccupiedError:
         await event.edit("This Username is Already Reserved")
 
+        
