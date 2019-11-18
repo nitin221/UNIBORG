2ebb634c320566d9ffbf9f6b737956c4f1d38252
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-18 08:55:24 +0530

    Update username.py

--- stdplugins/username.py
+++ stdplugins/username.py
@@ -3,22 +3,22 @@ from telethon import events
 from uniborg.util import admin_cmd
 
 from telethon.tl import functions
-from telethon.tl.functions.account import UpdateUsernameRequest
-from telethon.errors.rpcerrorlist import UsernameOccupiedError
+
+
 
 
 USERNAME_UPDATED = "```Username Successfully Updated.```"
-USERNAME_DENIED = "```This Username Already Reserved.```"
-
 
 
 @borg.on(admin_cmd(pattern="usr"))
 async def update_username(username):
 
-    newusername = events.pattern_match.group(1)
+    newusername = event.pattern_match.group(1)
     try:
-        await borg(UpdateUsernameRequest(newusername))
-        await username.edit(USERNAME_UPDATED)
+        await borg(functions.account.UpdateUsernameRequest(  # pylint:disable=E0602
+            about=username
+        
+        await event.edit(USERNAME UPDATED)
     except UsernameOccupiedError:
-        await username.edit(USERNAME_DENIED)
+        await event.edit(This Username is Already Reserved)
 
